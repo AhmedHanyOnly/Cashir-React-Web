d255fc965b165f3871763b1c261a61d438408ab1
ahmed <mommentcraft@gmail.com>
2025-09-27 21:43:07 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Cashier-Website/",
+  base: "/Cashir-React-Web/",
   optimizeDeps: {
     esbuildOptions: {
       define: {
