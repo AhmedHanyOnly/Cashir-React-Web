527473fef5bd7a561b3c4a889f415dc24cde2ecf
Mahmoud Mohammed <Mahmoudmohammed500@users.noreply.github.com>
2025-09-27 18:17:38 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/React-Cashier-Website/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // تقسيم الـ vendor libraries لملف منفصل
-          vendor: ["react", "react-dom", "react-router-dom", "react-bootstrap"],
-        },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
       },
     },
-    chunkSizeWarningLimit: 1000, // 1000 KB
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
